a455c7f3e330ddfe2a4b18006d4ea038403653df
kazuki.natsume <natsume.business@gmail.com>
2022-06-25 15:08:42 +0000

    post

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,33 @@
+class PostsController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
+  
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to root_url
+    else
+      @pagy, @posts = pagy(current_user.posts.order(id: :desc))
+      render 'toppages/index'
+    end
+  end
+
+  def destroy
+    @post.destroy
+    flash[:success] = 'メッセージを削除しました。'
+    redirect_back(fallback_location: root_path)
+  end
+  
+  private
+  
+  def post_params
+    params.require(:post).permit(:content, :image2)
+  end
+  
+  def correct_user
+    @post = current_user.posts.find_by(id: params[:id])
+    unless @post
+      redirect_to root_url
+    end
+  end
+end
